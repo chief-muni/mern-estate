42798ac56c4d924d4753ab5da802a19cb700d8e9
real-studios-web <web@realstudiosonline.com>
2024-08-10 17:06:01 +0000

    initial commit - installed react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500 underline underline-offset-[20px]">MERN Estate Project</h1>
+    </div>
+  )
+}
+
+export default App
