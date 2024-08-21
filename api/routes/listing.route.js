031cfe6a85a07e1def577c5008276ba8daad0d02
chief-muni <muni999@live.com>
2024-08-21 16:36:09 +0000

    create updateListing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createListing, deleteListing } = require('../controllers/listing.controller');
+const { createListing, deleteListing, updateListing } = require('../controllers/listing.controller');
 const verifyToken = require('../utils/verifyUser');
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.patch('/update/:id', verifyToken, updateListing);
 
 module.exports = router;
