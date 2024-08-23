24261c5cdb383e3c4046d8027a1a22334639a2e1
chief-muni <muni999@live.com>
2024-08-23 23:00:50 +0000

    complete header search from functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import { HiHomeModern, HiMiniMagnifyingGlass } from "react-icons/hi2";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
-  const { currentUser } = useSelector(state => state.user);
+  const 
+    { currentUser } = useSelector(state => state.user),
+    [searchTerm, setSearchTerm] = useState(''),
+    navigate = useNavigate()
+  ;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Constructing search params
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200  border-b border-gray-300  w-full" >
@@ -14,12 +34,16 @@ function Header() {
             <span className="text-slate-700">RD <span className="hidden sm:inline-block">Estates</span> </span>
           </h1>
         </Link>
-        <form className="flex items-center bg-slate-100 p-3 rounded-lg">
+        <form onSubmit={handleSubmit} className="flex items-center bg-slate-100 p-3 rounded-lg">
           <input type="text" id="search" 
             className="w-24 sm:w-64 bg-transparent focus:outline-none" 
             placeholder="Search..." 
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
           />
-          <HiMiniMagnifyingGlass />
+          <button>
+            <HiMiniMagnifyingGlass />
+          </button>
         </form>
         <ul className="flex gap-4 items-center">
           <li className="hidden sm:inline text-slate-700 hover:underline hover:underline-offset-4 cursor-pointer"><Link to="/">Home</Link></li>
