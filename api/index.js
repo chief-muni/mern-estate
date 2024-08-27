698468481aa96c11ca8bc13966f7af249ef9afab
chief-muni <muni999@live.com>
2024-08-27 13:43:53 +0000

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const 
   express = require('express'),
   cookieParser = require('cookie-parser'),
-  cors = require('cors')
+  cors = require('cors'),
+  path = require('path')
 ;
 const
   app = express(),
   connectDb = require('./utils/db'),
-  PORT = process.env.PORT || 5001
+  PORT = process.env.PORT || 5001,
+  baseDir = path.resolve();
 ;
 connectDb();
 app.use(express.json());
@@ -15,14 +17,17 @@ app.use(cookieParser());
 app.use(cors({
   credentials: true,
   origin: 'http://192.168.101.143:5173'   // Office network
-  // origin: 'http://192.168.94.232:5173'   // mobile network
 }));
 if(process.env.NODE_ENV === 'development') app.use(require('morgan')('dev'));
 
 app.use('/api/v1/user', require('./routes/user.route'));
 app.use('/api/v1/auth', require('./routes/auth.route'));
 app.use('/api/v1/listing', require('./routes/listing.route'));
-
+// Linking backend to frontend
+app.use(express.static(path.join(baseDir, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(baseDir, 'client', 'dist', 'index.html'));
+});
 // Creating error handler below all routes to enable it receive the next
 app.use((err, req, res, next) => {
   const 
