66c1f628623411f6d3be08bf5a79024039385cbc
chief-muni <muni999@live.com>
2024-08-27 12:38:56 +0000

    complete about page and wrap up front end development

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,10 @@
 function About() {
   return (
-    <div>
-      About us
+    <div className="container py-20 px-4">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About RD Estates</h1>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur quod at, mollitia, tempore soluta reiciendis culpa earum non ad similique sapiente saepe vero unde voluptatibus magni blanditiis qui totam quas, nisi dolorum animi ea voluptatem est quo. Dolores nemo at laudantium aspernatur reprehenderit aliquid similique, quasi impedit, in et quos.</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam explicabo sequi maxime sapiente autem doloribus rem soluta rerum iure maiores, excepturi totam, impedit suscipit laborum repellat expedita perspiciatis quis officia veniam recusandae? Porro tenetur cum dolores modi sit sunt, commodi minima, similique veritatis fugiat autem doloribus aspernatur laudantium. Ad repudiandae dolores alias aut nihil cum molestias odit magni illum quod?</p>
+      <p className="mb-4 text-slate-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime expedita, veniam voluptas earum id numquam quo, pariatur repudiandae saepe eligendi incidunt iste porro blanditiis quidem eaque est praesentium atque fuga doloribus, quos in aliquid?</p>
     </div>
   )
 }
